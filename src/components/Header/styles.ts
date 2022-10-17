2ee7e570fe0fd4d5b0f9a0a930ce4df4a3df37cb
Martin <escorpmartin97@gmail.com>
2022-10-17 13:49:48 -0300

    feat: importa logo da app

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  margin: 0 auto;
+  padding: 4rem;
+  height: 18.5rem;
+`
--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
 export const LayoutContainer = styled.header`
-  
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
 `
