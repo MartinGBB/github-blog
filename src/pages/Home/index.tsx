4012c1726304563adfeddd0af86a421634b161ea
Martin <escorpmartin97@gmail.com>
2022-10-19 23:33:09 -0300

    refator: muda de componente a requisição dos dados do perfil

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,41 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { Loading } from '../../components/Loading'
 import { PostContext } from '../../contexts/PostContext'
+import { api } from '../../lib/axios'
 import { Post } from './Post'
 import { Profile } from './Profile'
 import { SearchForm } from './SearchForm'
 import { HomeContainer, PostsContent } from './styles'
 
+export interface UserDataProfile {
+  avatar_url: string
+  name: string
+  bio: string
+  html_url: string
+  login: string
+  company: string
+  followers: number
+}
+
 export function Home() {
   const { postList } = useContext(PostContext)
+
+  const [userData, setUserData] = useState<UserDataProfile>()
+
+  async function fetchUser() {
+    const response = await api.get('users/MartinGBB')
+    setUserData(response.data)
+  }
+
+  useEffect(() => {
+    fetchUser()
+  }, [])
+
+  if (!userData?.avatar_url || !postList[0]?.title) return <Loading />
+
   return (
     <HomeContainer>
-      <Profile />
+      <Profile user={userData} />
       <SearchForm />
 
       <PostsContent>
